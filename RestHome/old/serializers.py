c93b685cac0b286874d022a9b66868aba3ca9053
张钧博 <5525480+mixisama@user.noreply.gitee.com>
2020-05-02 18:33:50 +0800

    增加查询老人接口一个小字段

--- RestHome/old/serializers.py
+++ RestHome/old/serializers.py
@@ -5,4 +5,4 @@ from old.models import Old
 class OldSerializer(serializers.ModelSerializer):
     class Meta:
         model = Old
-        fields = ['username', 'first_name', 'sex', 'room', 'telephone', 'address']
+        fields = ['date_joined', 'username', 'first_name', 'sex', 'room', 'telephone', 'address']
